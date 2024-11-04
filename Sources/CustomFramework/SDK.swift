a7fd28f33509afed6e8841ba9e3bc02cf6a608c8
CeciliaChen <cecilia.g.chen@outlook.com>
2024-11-04 23:48:12 +0800

    init

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Cecilia Chen on 11/4/24.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
